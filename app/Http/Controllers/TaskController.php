adce4208e2faa6633f4ecf08a32c3c939d609f0a
makiron918 <gonzaress918@gmail.com>
2020-06-10 17:34:10 +0900

    change controller
    コントローラの記述を変更した。

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    /**
+     * 新しいコントローラインスタンスの生成
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+}
